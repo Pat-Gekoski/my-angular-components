88839c14b7730aa0fbef120794b330e801a79e6e
Patric Gekoski <=>
2025-03-09 21:09:46 -0400

    SingleSelectComponent

--- src/app/routes/form-components/form-components.component.ts
+++ src/app/routes/form-components/form-components.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, signal } from '@angular/core'
 import { ComponentCardComponent } from '../../shared/component-card/component-card.component'
 import { InputComponent } from './input/input.component'
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
+import { SingleSelectComponent } from './single-select/single-select.component'
 
 @Component({
 	selector: 'app-form-components',
 	standalone: true,
-	imports: [ComponentCardComponent, InputComponent, ReactiveFormsModule, FormsModule],
+	imports: [ReactiveFormsModule, FormsModule, ComponentCardComponent, InputComponent, SingleSelectComponent],
 	templateUrl: './form-components.component.html',
 	styleUrl: './form-components.component.scss',
 })
@@ -17,12 +18,14 @@ export class FormComponents {
 
 	public form: FormGroup = this.fb.group({
 		appInput: this.fb.control('', [Validators.required, Validators.minLength(3)]),
+		appSingleSelect: this.fb.control('', [Validators.required]),
 	})
 
 	public onDisabledChanged(event: any): void {
 		const disabled = event.target.checked
 		this.isDisabled.set(disabled)
 		this.form.controls['appInput'][this.isDisabled() ? 'disable' : 'enable']()
+		this.form.controls['appSingleSelect'][this.isDisabled() ? 'disable' : 'enable']()
 	}
 
 	public onFloatingLabelChanged(event: any): void {
